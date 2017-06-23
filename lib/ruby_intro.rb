8c4d1ae85fada40012a385e3c3c0d40803087d7a
idris <fire-cat@hotmail.fr>
2017-06-23 20:29:12 +0000

    Updated files after pairing session

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -7,21 +7,24 @@ def sum arr
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  return 0 if arr.empty?
+  return arr[0] if arr.length == 1
+  arr.sort[-2] + arr.sort[-1]
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  # 
 end
 
-# Part 2
+# Part 2 
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s.match(/^[a-z&&[^aeoui]]/i).nil? then false else true end
 end
 
 def binary_multiple_of_4? s
@@ -31,5 +34,26 @@ end
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize isbn, price
+    raise ArgumentError if isbn.empty? or price <= 0
+    @isbn = isbn 
+    @price = price
+  end
+  
+  attr_reader :isbn, :price
+  
+  def isbn= isbn 
+    raise ArgumentError if isbn.empty?
+    @isbn = isbn
+  end
+  
+  def price= price
+    raise ArgumentError if price <= 0
+    @price = price
+  end
+  
+  def price_as_string
+    "$#{'%.2f' % price }"
+  end
+    
 end
